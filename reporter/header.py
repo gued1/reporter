c7c0657d38f157e98db5b546c698823a40558915
Stephane Varenne <Steph@MacBook-Pro-de-Stephane.local>
2019-07-11 11:43:18 +0200

    Mon premier commit

---
+++ reporter/header.py
@@ -0,0 +1,39 @@
+import warnings
+import datetime
+
+def header(authors, place='Paris'):
+    """Create the header text on the authors list
+    Parameters
+    ----------
+    authors : A list of dictionnaries and each dictionnary have "firstname" & "lastname" keys
+    place : To add dynamic city
+    
+    Returns
+    -------
+    liste : str which is the header text
+    
+    Note 
+    ----
+    
+    Date is updated at the function execution time
+    
+    """
+    ##Definition of date
+    today = datetime.date.today()
+    date = today.strftime("%d/%m/%y")
+    liste = []
+    liste.append(f"{place}, le {date}\n {len(authors)} auteurs:\n")
+    ##Get the author names
+    for element in authors:
+        try:
+            nom = element["firstname"]
+        except KeyError:
+            nom = 'XXX'
+            warnings.warn('Firstname is missing')
+        
+        prenom = element.get('lastname', 'XXX')
+        
+        
+        liste.append(f"- {nom} {prenom}")
+    ##print("\n".join(liste))
+    return("\n".join(liste))
